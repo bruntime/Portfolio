0c780d4cfdc59828a243bb9f0884c0eac9051dbd
ANNIE <bruntime@gmail.com>
2018-03-09 22:39:23 -0600

    Project Euler - Problem 1 Multipes of 3 and 5
    Completed Project Euler problem 1 using C# (https://projecteuler.net/problem=1)

---
+++ Challenges/Project Euler/C#/1. multiples 3 and 5/1. multiples 3 and 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.multiples_3_and_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Problem: Multiples of 3 and 5 - https://projecteuler.net/problem=1
+
+            // If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+            // we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+            // Find the sum of all the multiples of 3 or 5 below 1000.
+
+            IEnumerable<int> numTo1000 = Enumerable.Range(1, 999);
+
+            int sumNumDivisibleBy3Or5 = 0;
+
+            foreach (int num in numTo1000)
+            {
+                if (num % 3 == 0 || num % 5 == 0)
+                {
+                    sumNumDivisibleBy3Or5 += num;
+                }
+            }
+            Console.WriteLine("Sum of all multiples of 3 or 5 below 1000: " + sumNumDivisibleBy3Or5);
+        }
+    }
+}
