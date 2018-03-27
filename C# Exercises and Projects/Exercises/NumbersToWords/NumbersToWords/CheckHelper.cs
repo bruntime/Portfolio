138f7a17a2fd0e2c9f594df80ad9477392b22228
ANNIE <bruntime@gmail.com>
2018-03-26 23:01:03 -0500

    Numbers to Words (in progress)

---
+++ C# Exercises and Projects/Exercises/NumbersToWords/NumbersToWords/CheckHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersToWords
+{
+    public class CheckHelper
+    {
+        static void Main(string[] args) // static with a void (or int) return type
+        {
+        }
+
+        public string ConvertZeroToNine(decimal num)
+        {
+            //for each number from 0 - 9, replace with word
+            //if num is equal to 0 replace with "zero"
+            //for 1 replace with "one"
+            //for 2 replace with "two"
+            //return each value
+            if (num == 0) return "zero";
+            if (num == 1) return "one";
+            if (num == 2) return "two";
+            if (num == 3) return "three";
+            if (num == 4) return "four";
+            if (num == 5) return "five";
+            if (num == 6) return "six";
+            if (num == 7) return "seven";
+            if (num == 8) return "eight";
+            if (num == 9) return "nine";
+
+            return null;
+        }
+
+        public string ConvertNumbersToWords(decimal num)
+        {
+            return null;
+            //10 - 19
+            //20 - 99
+            //100 - 999
+            //1000 - 9999
+        }
+    }
+}
---
+++ C# Exercises and Projects/Exercises/NumbersToWords/NumbersToWordsTest/NumbersTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumbersToWords;
+
+namespace NumbersToWordsTest
+{
+    [TestClass]
+    public class NumbersTest
+    {
+        [TestMethod]
+        public void ZeroToNineTests()
+        {
+            //-- Arrange
+            CheckHelper checkZeroToNine = new CheckHelper();
+            string expected = checkZeroToNine.ConvertZeroToNine(0);
+            //-- Act
+            string actual = "zero";
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
+            //0 - 9 tests
+            Assert.AreEqual(checkZeroToNine.ConvertZeroToNine(1), "one");
+            Assert.AreEqual(checkZeroToNine.ConvertZeroToNine(2), "two");
+            Assert.AreEqual(checkZeroToNine.ConvertZeroToNine(3), "three");
+            Assert.AreEqual(checkZeroToNine.ConvertZeroToNine(4), "four");
+            Assert.AreEqual(checkZeroToNine.ConvertZeroToNine(5), "five");
+            Assert.AreEqual(checkZeroToNine.ConvertZeroToNine(6), "six");
+            Assert.AreEqual(checkZeroToNine.ConvertZeroToNine(7), "seven");
+            Assert.AreEqual(checkZeroToNine.ConvertZeroToNine(8), "eight");
+            Assert.AreEqual(checkZeroToNine.ConvertZeroToNine(9), "nine");
+
+        }
+    }
+}
