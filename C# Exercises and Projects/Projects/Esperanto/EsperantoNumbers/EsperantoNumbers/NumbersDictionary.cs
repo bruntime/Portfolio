3926081c71eb23f1ce75ab7bbf57fd8a868d5e34
ANNIE <bruntime@gmail.com>
2018-04-03 20:30:24 -0500

    Numbers Dictionary Converter v1.5
    - converts each digit to its corresponding value correctly
    - test need to be created

---
+++ C# Exercises and Projects/Projects/Esperanto/EsperantoNumbers/EsperantoNumbers/NumbersDictionary.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsperantoNumbers
+{
+    public class NumbersDictionary
+    {
+
+        public long UserInput()
+        {
+            Console.WriteLine("Input a number: ");
+            long userInput = long.Parse(Console.ReadLine());
+            return userInput;
+        }
+
+        public string NumbersToWords()
+        {
+            string userInput = UserInput().ToString();
+            //Esperanto base numbers
+            var esperantoNumsDict = new Dictionary<string, string>()
+            {
+                { "0", "nulo" },
+                { "1", "unu" },
+                { "2", "du" },
+                { "3", "tri" },
+                { "4", "kvar" },
+                { "5", "kvin" },
+                { "6", "ses" },
+                { "7", "sep" },
+                { "8", "ok" },
+                { "9", "nau" },
+                { "10", "dek"},
+                { "100", "cent"},
+                { "1000", "mil"},
+                { "1000000", "miiono"},
+                { "1000000000", "milionardo"},
+                { "1000000000000", "bilionardo"}
+            };
+
+            var keys = esperantoNumsDict.Keys;
+            var values = esperantoNumsDict.Values;
+
+            //take user input and convert to characters
+            char[] userInputConvertToString = userInput.ToString().ToCharArray();
+
+            foreach (var num in userInputConvertToString)
+            {
+                //convert each character digit to a string
+                var numString = num.ToString();
+                foreach (var key in keys)
+                {
+                    var stringKey = key.ToString();
+                    if (numString == stringKey)
+                    {
+                        var stringValue = values.ToString();
+                        Console.WriteLine(numString + " is " + esperantoNumsDict[stringKey]);
+                    }
+                }
+            }
+            Console.ReadKey();
+
+            return null;
+        }
+    }
+}
--- C# Exercises and Projects/Projects/Esperanto/EsperantoNumbers/EsperantoNumbersTest/NumbersTest.cs
+++ C# Exercises and Projects/Projects/Esperanto/EsperantoNumbers/EsperantoNumbersTest/NumbersTest.cs
@@ -8,8 +8,27 @@ namespace EsperantoNumbersTest
     public class NumbersTest
     {
         [TestMethod]
+        public void UserInputValid()
+        {
+            //test that user input is valid
+
+            //-- Arrange
+
+            //-- Act
+
+            //-- Assert
+        }
         public void NumbersToWordsValid()
         {
+            //test that an actual value returns as expected value
+            
+            //-- Arrange
+            var testInstance = new NumbersDictionary();
+
+            //-- Act
+            testInstance.NumbersToWords();
+
+            //-- Assert
 
         }
     }
