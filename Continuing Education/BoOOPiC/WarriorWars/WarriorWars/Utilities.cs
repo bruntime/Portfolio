5630936f9ce21ed26336faa96bfb28eadfffeae9
ANNIE <bruntime@gmail.com>
2018-04-06 11:11:08 -0500

    Warrior Wars Game v1.0
    A console application that puts together all the concepts learned in the Basics of Object-Oriented Programming in C# course

---
+++ Continuing Education/BoOOPiC/WarriorWars/WarriorWars/Utilities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WarriorWars
+{
+    static class Utilities
+    {
+        public static void ColorfulWriteLine(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Continuing Education/BoOOPiC/WarriorWars/WarriorWars/Warrior.cs
@@ -0,0 +1,79 @@
+﻿using WarriorWars.Enum;
+using WarriorWars.Equipment;
+using System;
+using System.Threading;
+
+namespace WarriorWars
+{
+    class Warrior
+    {
+        private const int GOOD_GUY_STARTING_HEALTH = 100;
+        private const int BAD_GUY_STARTING_HEALTH = 100;
+
+        private readonly Faction FACTION;
+
+        private int health;
+        private string name;
+        private bool isAlive;
+
+        private Weapon weapon;
+        private Armor armor;
+
+        public bool IsAlive
+        {
+            get
+            {
+                return isAlive;
+            }
+         }
+
+        public Warrior(string name, Faction faction)
+        {
+            this.name = name;
+            FACTION = faction;
+
+            isAlive  = true; //when initially created
+
+            switch (faction)
+            {
+                case Faction.GoodGuy:
+                    weapon = new Weapon(faction);
+                    armor = new Armor(faction);
+                    health = GOOD_GUY_STARTING_HEALTH;
+                    break;
+                case Faction.BadGuy:
+                    weapon = new Weapon(faction);
+                    armor = new Armor(faction);
+                    health = BAD_GUY_STARTING_HEALTH;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Attack(Warrior enemy)
+        {
+            int damage = weapon.Damage / enemy.armor.ArmorPoints;
+
+            enemy.health -= damage;
+
+            AttackResult(enemy, damage);
+        }
+
+        private void AttackResult(Warrior enemy, int damage)
+        {
+            if (enemy.health <= 0)
+            {
+                enemy.isAlive = false;
+                Utilities.ColorfulWriteLine($"{enemy.name} is dead!", ConsoleColor.Red);
+                Utilities.ColorfulWriteLine($"{name} is victorious", ConsoleColor.Green);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine($"{name} attacked {enemy.name}. {damage} was inflicted to {enemy.name}, remaining health of {enemy.name} is {enemy.health}");
+            }
+            Thread.Sleep(200);
+        }
+    }
+}
