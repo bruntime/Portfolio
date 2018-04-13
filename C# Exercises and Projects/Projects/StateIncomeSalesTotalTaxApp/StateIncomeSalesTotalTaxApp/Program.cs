8f3badd0cd520fe1e1250dee00fec3d01abe5d85
Annie <bruntime@gmail.com>
2018-04-12 22:42:40 -0500

    State Tax and Ranking App v1.0 (Complete)
    - created state info class which contains tax information for each state
    - outputting information in console for each state
    - refactored code, added ids to tax data for each table cell
    - included images for complete console application
    
    - To Do: Allow users to search by state, filter by ranking, compare 2 states, create a desktop version, create a mobile application version

--- C# Exercises and Projects/Projects/StateIncomeSalesTotalTaxApp/StateIncomeSalesTotalTaxApp/Program.cs
+++ C# Exercises and Projects/Projects/StateIncomeSalesTotalTaxApp/StateIncomeSalesTotalTaxApp/Program.cs
@@ -7,11 +7,9 @@ namespace StateIncomeSalesTotalTaxApp
 {
     class Program
     {
-        private const string stateDataCell = "<td class=\"tease\" style=\"font-weight: bold;\">";
-
         static void Main(string[] args)
         {
-            Console.WriteLine("State Income, Sales, and Total Tax & Rankings");
+            Console.WriteLine("2002 State Income, Sales, and Total Tax & Rankings");
 
             WebClient client = new WebClient();
 
@@ -19,37 +17,96 @@ namespace StateIncomeSalesTotalTaxApp
             string result = client.DownloadString("http://money.cnn.com/pf/features/lists/total_taxes/");
             string fileName = "CNN State Tax Info";
 
-            if (File.Exists(String.Format(@"C:~\Documents\Development\CNN Tax Info\{0}", fileName)))
+            string fileLocationExtension = "\\Users\\User";
+
+            if (File.Exists(String.Format(@"C:{0}\Documents\Development\CNN Tax Info\{1}", fileLocationExtension, fileName)))
             {
-                Console.WriteLine("The file: {0}, already exists", fileName);
+                //Console.WriteLine("The file: {0}, already exists", fileName);
             }
             else
             {
-                File.WriteAllText(String.Format(@"C:~\Documents\Development\CNN Tax Info\{0}", fileName), result);
+                File.WriteAllText(String.Format(@"C:{0}\Documents\Development\CNN Tax Info\{1}", fileLocationExtension, fileName), result);
                 Console.WriteLine("{0} file was just created", fileName);
             }
 
-            List<string> states = new List<string>();
+            StateInfo stateInfo = new StateInfo();
             string line;
 
             using (StreamReader file = new StreamReader(@"C:~\Documents\Development\CNN Tax Info\CNN State Tax Info"))
             {
                 while ((line = file.ReadLine()) != null)
                 {
-                    if (line.Contains(stateDataCell))
+                    if (line.Contains("font-weight: bold"))
                     {
-                        states.Add(line);
+                        line = line.Split('>', '/')[1];
+                        line = line.TrimEnd('<');
+                        stateInfo.states.Add(line);
+                    }
+                    //PIT - Personal Income Tax
+                    if (line.Contains("PIT"))
+                    {
+                        line = line.Split('>', '/')[1];
+                        line = line.TrimEnd('<');
+                        double personalIncomeTax = double.Parse(line);
+                        stateInfo.personalIncomeTax.Add(personalIncomeTax);
+                    }
+                    //PTR - Personal Income Tax Rank
+                    if (line.Contains("PTR"))
+                    {
+                        line = line.Split('>', '/')[1];
+                        line = line.TrimEnd('<');
+                        int personalIncomeTaxRank = int.Parse(line);
+                        stateInfo.personalIncomeTaxRank.Add(personalIncomeTaxRank);
+                    }
+                    //STR - Sales Tax
+                    if (line.Contains("ST\">"))
+                    {
+                        line = line.Split('>', '/')[1];
+                        line = line.TrimEnd('<');
+                        double salesTax = double.Parse(line);
+                        stateInfo.salesTax.Add(salesTax);
+                    }
+                    //STR - Sales Tax Rank
+                    if (line.Contains("SR\">"))
+                    {
+                        line = line.Split('>', '/')[1];
+                        line = line.TrimEnd('<');
+                        int salesTaxRank = int.Parse(line);
+                        stateInfo.salesTaxRank.Add(salesTaxRank);
+                    }
+                    //TT - Total Tax
+                    if (line.Contains("TT\">"))
+                    {
+                        line = line.Split('>', '/')[1];
+                        line = line.TrimEnd('<');
+                        double totalTax = double.Parse(line);
+                        stateInfo.totalTax.Add(totalTax);
+                    }
+                    //TR - Total Tax Rank
+                    if (line.Contains("TR\">"))
+                    {
+                        line = line.Split('>', '/')[1];
+                        line = line.TrimEnd('<');
+                        int totalTaxRank = int.Parse(line);
+                        stateInfo.totalTaxRank.Add(totalTaxRank);
                     }
                 }
             }
 
-            foreach (var state in states)
+            Console.WriteLine("* State * Personal Income Tax * Rank * Sales Tax * Rank * Total Tax * Rank *");
+
+            for (int i = 0; i < stateInfo.states.Count; i++)
             {
-                Console.WriteLine(state);
+                Console.WriteLine(stateInfo.states[i].PadRight(15) +
+                    stateInfo.personalIncomeTax[i].ToString().PadRight(18) +
+                    stateInfo.personalIncomeTaxRank[i].ToString().PadRight(8) + 
+                    stateInfo.salesTax[i].ToString().PadRight(10) +
+                    stateInfo.salesTaxRank[i].ToString().PadRight(9) +
+                    stateInfo.totalTax[i].ToString().PadRight(11) +
+                    stateInfo.totalTaxRank[i]);
             }
 
             Console.ReadKey();
-
         }
     }
 }
---
+++ C# Exercises and Projects/Projects/StateIncomeSalesTotalTaxApp/StateIncomeSalesTotalTaxApp/StateInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateIncomeSalesTotalTaxApp
+{
+    public class StateInfo
+    {
+        public List<string> states = new List<string>();
+        public List<double> personalIncomeTax = new List<double>();
+        public List<int> personalIncomeTaxRank = new List<int>();
+        public List<double> salesTax = new List<double>();
+        public List<int> salesTaxRank = new List<int>();
+        public List<double> totalTax = new List<double>();
+        public List<int> totalTaxRank = new List<int>();
+
+        public StateInfo()
+        {
+        }
+    }
+}
